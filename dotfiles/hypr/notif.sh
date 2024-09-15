d144e3a53f829febf33bbd8130e22813c8be55a7
bhu1-103 <103.bhuvan@gmail.com>
2024-09-16 02:22:12 +0530

    update at Mon Sep 16 02:22:12 AM IST 2024

---
+++ dotfiles/hypr/notif.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+mako --background-color "#000000" --border-color "#00FF00" --border-size "5" --height 200 --text-color "#FF0000" --width "600" --font "Gohu Font 14 Nerd Font 20" --anchor bottom-left --max-visible 3

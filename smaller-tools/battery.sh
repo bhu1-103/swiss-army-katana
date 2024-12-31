6f7d95f0c0664ceffe47b200987c03a204acb513
bhu1-103 <103.bhuvan@gmail.com>
2024-12-31 13:09:00 +0530

    Tue Dec 31 01:09:00 PM IST 2024 > smaller tools

---
+++ smaller-tools/battery.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/zsh
+
+export DISPLAY=:0
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
+battery_level=$(acpi | head -n 2 | tail -n 1 | awk '{print $4}' | tr -d ',%')
+
+if [ "$battery_level" -gt 75 ]; then
+	dunstify "Unplug charger" "Current battery level is $battery_level%"
+fi
+
+if [ "$battery_level" -lt 35 ]; then
+	dunstify "Battery Level" "Critical $battery_level%"
+fi

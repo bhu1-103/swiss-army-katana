0fda0c61b2dd9fa5bfc3afe46625f599aa9709ba
bhu1-103 <103.bhuvan@gmail.com>
2025-01-18 23:44:38 +0530

    Sat Jan 18 11:44:38 PM IST 2025

--- smaller-tools/battery.sh
+++ smaller-tools/battery.sh
@@ -2,12 +2,12 @@
 
 export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
-battery_level=$(acpi | head -n 2 | tail -n 1 | awk '{print $4}' | tr -d ',%')
+battery_level=$(acpi | head -n 1 | tail -n 1 | awk '{print $4}' | tr -d ',%')
 
 if [ "$battery_level" -gt 75 ]; then
 	dunstify "Unplug charger" "Current battery level is $battery_level%"
 fi
 
-if [ "$battery_level" -lt 35 ]; then
+if [ "$battery_level" -lt 45 ]; then
 	dunstify "Battery Level" "Critical $battery_level%"
 fi

79a21ded9ae908d87d9aeefa3048c894b4ed9285
bhu1-103 <103.bhuvan@gmail.com>
2025-03-06 23:58:28 +0530

    checking if file exists

--- smaller-tools/nvidia-shadow-play-compress.sh
+++ smaller-tools/nvidia-shadow-play-compress.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "Usage: $0 <input-file>"
+    exit 1
+fi
 input="$1"
 output="${1%.*}-re-encoded.mp4"
 ffmpeg -i "$input" -c:v libx265 -crf 28 -preset slow -c:a aac -b:a 128k "$output"

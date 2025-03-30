c104666e01836346bdacb331822dbf61072615ca
bhu1-103 <103.bhuvan@gmail.com>
2025-03-30 19:45:58 +0530

    mpd starts once aux port is connected

--- power-tools/battery-saver.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/zsh
-
-pacman -S cpupower tlp
-cpupower frequency-set -d 400MHz -u 1.0GHz
-systemctl enable tlp
-system disable bluetooth
--- tools/headphone-jack-detect.sh
+++ tools/headphone-jack-detect.sh
@@ -3,8 +3,11 @@
 alsactl monitor | while read -r line; do
     if [[ "$line" == *"Headphone Playback Switch"* ]]; then
         if amixer get Headphone | grep -q '\[off\]'; then
-            echo "Headphones unplugged, stopping MPD..."
-            mpc stop
+            echo "Headphones unplugged, pausing MPD..."
+            mpc pause
+        else
+            echo "Headphones plugged in, resuming MPD..."
+            mpc play
         fi
     fi
 done

ed44ddb7e401b492f93be3607ee6ac3618dc2046
bhu1-103 <103.bhuvan@gmail.com>
2025-04-01 23:53:07 +0530

    love2d begins

---
+++ love2d/main.lua
@@ -0,0 +1,23 @@
+function love.load()
+  love.window.setPosition(300,300,1)
+  aX = 0
+  aY = 0
+  love.graphics.setColor(255,0,0)
+end
+
+function love.update(dt)
+  down = love.mouse.isDown(1)
+end
+
+function love.draw()
+  cursorX,cursorY = love.mouse.getPosition()
+  if down == true then
+    aX = cursorX
+    aY = cursorY
+  end
+  love.graphics.print(tostring(down),10,10)
+  --love.graphics.print(cursorX,10,10)
+  --love.graphics.print(cursorY,10,30)
+  love.graphics.rectangle("fill",aX,aY,cursorX,cursorY)
+
+end

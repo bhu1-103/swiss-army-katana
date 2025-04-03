83b24942ff3b545491d69bf7096b4d58a7e2d664
bhu1-103 <103.bhuvan@gmail.com>
2025-04-04 00:39:57 +0530

    removed complications fr this time

--- dotfiles/mpd-scrolling-text.sh
+++ dotfiles/mpd-scrolling-text.sh
@@ -4,19 +4,29 @@ max_length=25
 delay=0.5
 
 while true; do
+    mpd_status=$(mpc status | grep -Eo '\[playing\]|\[paused\]')
     song=$(mpc current --format "%title% - %artist%")
-    
-    if [[ -z "$song" ]]; then
+
+    song="🎵 Now Playing: $song"
+
+    if [[ ${#song} -le $max_length ]]; then
+        json_output="{\"text\": \"$song\", \"tooltip\": \"$song\"}"
+        echo "$json_output" | jq -r '.text'
         sleep 1
-        continue
+    else
+        song="   $song   "
+        full_length=${#song}
+
+        for i in {1..$((full_length - max_length + 1))}; do
+            new_status=$(mpc status | grep -Eo '\[playing\]|\[paused\]')
+            new_song=$(mpc current --format "%title% - %artist%")
+
+            song="🎵 Now Playing: $new_song"
+
+            scroll_text=${song[$i,$((i + max_length - 1))]}
+            json_output="{\"text\": \"$scroll_text\", \"tooltip\": \"$song\"}"
+            echo "$json_output" | jq -r '.text'
+            sleep $delay
+        done
     fi
-
-    song="   $song   "
-    full_length=${#song}
-
-    for ((i = 0; i <= full_length - max_length; i++)); do
-        scroll_text=${song[i,max_length]}
-        echo "$scroll_text"
-        sleep $delay
-    done
 done

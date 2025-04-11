c78b6e471782c9de991e1cc8656ebfa3d1c22c93
bhu1-103 <103.bhuvan@gmail.com>
2025-04-11 13:11:43 +0530

    Fri Apr 11 01:11:43 PM IST 2025

--- home-lab/setup.sh
+++ home-lab/setup.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/zsh
 
-mkdir home-lab
-cd home-lab
-#wget https://cdimage.kali.org/kali-2025.1a/kali-linux-2025.1a-installer-amd64.iso #deprecated because kali is not ideal for quick a-install
-
 wget https://repo-default.voidlinux.org/live/current/void-live-x86_64-20250202-xfce.iso
 qemu-img create -f qcow2 image.img 10G
 

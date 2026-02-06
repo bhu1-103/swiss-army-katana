bd61d64603b7de23f29f92f8b4229bc19a02d3fe
bhu1-103 <103.bhuvan@gmail.com>
2026-02-06 23:52:23 +0530

    ttp224 touch sensor

--- imx500/ttp224/touch-demo-01.py
+++ imx500/ttp224/touch-demo-01.py
@@ -1,14 +1,41 @@
 from gpiozero import Button
 from signal import pause
+import time
 
-# BCM order from Left -> Right
 PAD_ORDER = [23, 22, 27, 17]
-
+SWIPE_WINDOW = 0.5
+MIN_PADS_IN_SWIPE = 3
+history = []
 pads = []
+
+def check_for_swipe():
+    global history
+    now = time.time()
+
+    history = [(t,i) for (t,i) in history if now - t < SWIPE_WINDOW]
+    indices = [i for (_,i) in history]
+
+    if len(indices) < MIN_PADS_IN_SWIPE:
+        return
+
+    cleaned = []
+    for i in indices:
+        if not cleaned or cleaned[-1] != i:
+            cleaned.append(i)
+
+    if cleaned == sorted(cleaned):
+        print(">>RIGHT>>")
+        history.clear()
+
+    if cleaned == sorted(cleaned, reverse=True):
+        print(">>LEFT>>")
+        history.clear()
 
 def make_handlers(index):
     def pressed():
         print(f"PAD {index+1} PRESSED")
+        history.append((time.time(),index))
+        check_for_swipe()
 
     def released():
         print(f"PAD {index+1} RELEASED")

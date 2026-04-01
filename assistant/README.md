b989fb0d4ce0a992f9b6bda85da696ad9eed64e0
bhu1-103 <103.bhuvan@gmail.com>
2026-04-01 23:44:26 +0530

    removed deprecated voice assistant

--- assistant/README.md
+++
@@ -1,7 +0,0 @@
-kokoro tts ftw
-
-vosk ftw
-
-no mic to use vosk
-
-check out a revised and better assistant [here](https://github.com/bhu1-103/alter-ego)
--- assistant/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-pip install vosk-transcriber
-wget https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip
-unzip vosk-model-small-en-us-0.15.zip
-wget https://github.com/bhu1-103/swiss-army-katana/blob/main/assistant/assistant.sh
